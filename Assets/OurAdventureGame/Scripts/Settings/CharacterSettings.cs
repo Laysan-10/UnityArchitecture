145594ca370da882ec3059d692961ff7be8ea3c2
Neco-tech907 <ivanovraul555@gmail.com>
2026-03-01 17:22:35 +0300

    .

--- Assets/OurAdventureGame/Scripts/Settings/CharacterSettings.cs
+++ Assets/OurAdventureGame/Scripts/Settings/CharacterSettings.cs
@@ -46,7 +46,7 @@ public class CharacterSettings : MonoBehaviour
     private void Start()
     {
         Debug.Log(selestch);
-        selestch = PlayerPrefs.GetInt("CharacterSelected"); // получаем значени по ключи из PlayerPrefs
+        selestch = PlayerPrefs.GetInt("CharacterSelected"); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ PlayerPrefs
         _virtualCameraComponent = _virtual_camera.GetComponent<CinemachineVirtualCamera>();
 
         _goblinsSrangAI = FindObjectsOfType<enemyAI>();
